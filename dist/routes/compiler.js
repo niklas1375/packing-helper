94199c0c6dd4ca4847e92ed7659f72c43a06c5b0
Niklas <niklas1375@gmail.com>
2022-09-01 17:04:24 +0200

    cleanup

--- dist/routes/compiler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.register = void 0;
-const modules_1 = require("../modules");
-const register = (app) => {
-    app.post('/compile', modules_1.compiler.compileListFromSelections);
-};
-exports.register = register;
-//# sourceMappingURL=compiler.js.map
--- dist/routes/todoist.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.register = void 0;
-const modules_1 = require("../modules");
-const register = (app) => {
-    app.post('/submitTasks', modules_1.todoist.submitTasks);
-};
-exports.register = register;
-//# sourceMappingURL=todoist.js.map
