3b322d9cc08347e44b3b84a2556edb4f7f0715c7
Niklas <niklas1375@gmail.com>
2022-09-08 08:59:17 +0200

    test submitting tasks

---
+++ __tests__/routes/todoist.spec.ts
@@ -0,0 +1,33 @@
+import request from "supertest";
+
+import app from "../../app";
+
+describe("Submit Tasks to todoist", () => {
+  /*
+   * Create basic packing list first and create todoist task second
+   */
+  test("Test submitting tasks to todoist", async () => {
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const compileRes = await request(app)
+      .post("/api/compile")
+      .send({})
+      .expect("Content-Type", /json/)
+      .expect(200);
+    const submitRes = await request(app)
+      .post("/api/submitTasks")
+      .send({
+        tripName: "Testtrip",
+        tripLength: 5,
+        tripBeginDate: tomorrow.toISOString(),
+        packingList: compileRes.body,
+      })
+      .expect("Content-Type", /json/)
+      .expect(201);
+    expect(submitRes.body).toEqual({
+      status: 201,
+      text: "Created",
+    });
+    // --> check created task (& delete) via api as well? Would need task ID in reply...
+  });
+});
