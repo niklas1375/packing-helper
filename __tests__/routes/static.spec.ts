c4fe906092188f1d0c70510529f4adafef40a3a2
Niklas <niklas1375@gmail.com>
2024-07-30 16:47:14 +0200

    update tests

--- __tests__/routes/static.spec.ts
+++ __tests__/routes/static.spec.ts
@@ -2,31 +2,25 @@ import request from "supertest";
 
 import app from "../../app";
 
-import static_accomodation from "../json/static/accomodation.json";
-import static_activities from "../json/static/activities.json";
-import static_transport from "../json/static/transport.json";
-import static_triptypes from "../json/static/triptypes.json";
-import static_weather from "../json/static/weather.json";
-
 describe("Static routes", () => {
   test("Get all accomodations", async () => {
     const res = await request(app).get("/api/accomodation");
-    expect(res.body).toEqual(static_accomodation);
+    expect(res.body).toMatchSnapshot();
   });
   test("Get all activities", async () => {
     const res = await request(app).get("/api/activities");
-    expect(res.body).toEqual(static_activities);
+    expect(res.body).toMatchSnapshot();
   });
   test("Get all transports", async () => {
     const res = await request(app).get("/api/transport");
-    expect(res.body).toEqual(static_transport);
+    expect(res.body).toMatchSnapshot();
   });
   test("Get all triptypes", async () => {
     const res = await request(app).get("/api/triptypes");
-    expect(res.body).toEqual(static_triptypes);
+    expect(res.body).toMatchSnapshot();
   });
   test("Get all weathers", async () => {
     const res = await request(app).get("/api/weather");
-    expect(res.body).toEqual(static_weather);
+    expect(res.body).toMatchSnapshot();
   });
 });
