3edce299accd743a8de191d3a148e8036f9121f0
Niklas <niklas1375@gmail.com>
2024-09-23 15:05:50 +0200

    add CRUD routes

--- modules/static.ts
+++ modules/static.ts
@@ -1,40 +1,153 @@
 import { Request, Response } from "express";
-import { findPackingLists } from "../db/PackingListRepository";
-import { PackingList } from "../types/db/types";
+import { createId } from "@paralleldrive/cuid2";
+import {
+  findPackingListById,
+  findPackingLists,
+  deletePackingList as deletePackingListOnDb,
+  updatePackingList as updatePackingListOnDb,
+  createPackingList,
+} from "../db/PackingListRepository";
+import {
+  NewPackingItem,
+  NewPackingList,
+  PackingItemUpdate,
+  PackingList,
+  PackingListUpdate,
+} from "../types/db/types";
+import {
+  createPackingItem,
+  deletePackingItem,
+  findPackingItemsForList,
+  updatePackingItem,
+} from "../db/PackingItemRepository";
 
-async function getAccomodationTypes(_: Request, res: Response) {
-  const accomodations = await findPackingLists({
-    type: "accomodation"
-  });
-  res.json(_getTypesFromJSON(accomodations));
+// mass GET for packing lists
+export function getPackingListsOfType(type: string) {
+  return async function (_: Request, res: Response) {
+    const packingLists = await findPackingLists({
+      type: type,
+    });
+    res.json(_getTypesFromJSON(packingLists));
+  };
 }
 
-async function getActivityTypes(_: Request, res: Response) {
-  const activities = await findPackingLists({
-    type: "activity"
-  });
-  res.json(_getTypesFromJSON(activities));
+// single GET for packing lists
+export async function getSinglePackingList(req: Request, res: Response) {
+  const packingList = await findPackingListById(req.params.listId);
+  res.json(packingList);
 }
 
-async function getTransportTypes(_: Request, res: Response) {
-  const transport = await findPackingLists({
-    type: "transport"
-  });
-  res.json(_getTypesFromJSON(transport));
+// mass GET for packing list items
+export async function getItemsForPackingList(req: Request, res: Response) {
+  const packingItems = await findPackingItemsForList(req.params.listId);
+  res.json(packingItems);
 }
 
-async function getTripTypes(_: Request, res: Response) {
-  const triptypes = await findPackingLists({
-    type: "triptype"
-  });
-  res.json(_getTypesFromJSON(triptypes));
+// creation POST for packing lists
+export function createPackingListOfType(type: string) {
+  return async function (req: Request, res: Response) {
+    const newPackingList: NewPackingList = req.body;
+    newPackingList.type = type;
+    newPackingList.id = createId();
+    newPackingList.updated_at = new Date();
+    try {
+      const createdList = await createPackingList(newPackingList);
+      res.status(201);
+      res.json(createdList);
+    } catch (error) {
+      console.error(error);
+      res.status(500);
+      res.send("Internal Server Error");
+    }
+  };
 }
 
-async function getWeatherTypes(_: Request, res: Response) {
-  const weather = await findPackingLists({
-    type: "weather"
-  });
-  res.json(_getTypesFromJSON(weather));
+// creation POST for packing list items
+export async function createPackingItemForList(req: Request, res: Response) {
+  const newPackingItem: NewPackingItem = req.body;
+  newPackingItem.listId = req.params.listId;
+  newPackingItem.item_id = createId();
+  newPackingItem.updated_at = new Date();
+  try {
+    const createdItem = await createPackingItem(newPackingItem);
+    res.status(201);
+    res.json(createdItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+    res.send("Internal Server Error");
+  }
+}
+
+// update PATCH for packing list
+export async function updatePackingList(req: Request, res: Response) {
+  const updateId = req.params.listId;
+  // name should be the only updateable property
+  if (!req.body.name) {
+    res.status(400);
+    res.send({
+      message: "only name can be updated",
+    });
+  }
+  const updateObject: PackingListUpdate = {
+    name: req.body.name,
+  };
+  try {
+    await updatePackingListOnDb(updateId, updateObject);
+    res.status(204);
+    res.send();
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+    res.send("Internal Server Error");
+  }
+}
+
+// update PATCH for packing list item
+export async function updatePackingItemForList(req: Request, res: Response) {
+  const updateId = req.params.itemId;
+  const updateObject: PackingItemUpdate = req.body;
+  delete updateObject.item_id;
+  delete updateObject.listId;
+  updateObject.updated_at = new Date();
+  try {
+    await updatePackingItem(updateId, updateObject);
+    res.status(204);
+    res.send();
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+    res.send("Internal Server Error");
+  }
+  throw new Error("Function not implemented.");
+}
+
+// DELETE for packing list
+export async function deletePackingList(req: Request, res: Response) {
+  const deletionId = req.params.listId;
+  try {
+    await deletePackingListOnDb(deletionId);
+    res.status(204);
+    res.send();
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+    res.send("Internal Server Error");
+  }
+}
+
+// DELETE for packing list item
+export async function deletePackingItemFromList(req: Request, res: Response) {
+  const deletionId = req.params.itemId;
+  try {
+    await deletePackingItem(deletionId);
+    res.status(204);
+    res.send();
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+    res.send("Internal Server Error");
+  }
 }
 
 function _getTypesFromJSON(typesJSON: PackingList[]) {
@@ -50,11 +163,3 @@ function _getTypesFromJSON(typesJSON: PackingList[]) {
       return itemA.name > itemB.name ? 1 : -1;
     });
 }
-
-export {
-  getAccomodationTypes,
-  getActivityTypes,
-  getTransportTypes,
-  getTripTypes,
-  getWeatherTypes,
-};
--- routes/static.ts
+++ routes/static.ts
@@ -2,9 +2,58 @@ import { Router } from "express";
 import { staticContent } from '../modules'
 
 export const register = (app: Router) => {
-    app.get('/accomodation', staticContent.getAccomodationTypes);
-    app.get('/activities', staticContent.getActivityTypes);
-    app.get('/transport', staticContent.getTransportTypes);
-    app.get('/triptypes', staticContent.getTripTypes);
-    app.get('/weather', staticContent.getWeatherTypes);
+    // accomodation routes
+    app.get('/accomodation', staticContent.getPackingListsOfType("accomodation"));
+    app.get('/accomodation/:listId', staticContent.getSinglePackingList);
+    app.get('/accomodation/:listId/items', staticContent.getItemsForPackingList);
+    app.post('/accomodation', staticContent.createPackingListOfType("accomodation"));
+    app.post('/accomodation/:listId/items', staticContent.createPackingItemForList);
+    app.patch('/accomodation/:listId', staticContent.updatePackingList);
+    app.patch('/accomodation/:listId/items/:itemId', staticContent.updatePackingItemForList);
+    app.delete('/accomodation/:listId', staticContent.deletePackingList);
+    app.delete('/accomodation/:listId/items/:itemId', staticContent.deletePackingItemFromList);
+
+    // activity routes
+    app.get('/activities', staticContent.getPackingListsOfType("activity"));
+    app.get('/activities/:listId', staticContent.getSinglePackingList);
+    app.get('/activities/:listId/items', staticContent.getItemsForPackingList);
+    app.post('/activities', staticContent.createPackingListOfType("activity"));
+    app.post('/activities/:listId/items', staticContent.createPackingItemForList);
+    app.patch('/activities/:listId', staticContent.updatePackingList);
+    app.patch('/activities/:listId/items/:itemId', staticContent.updatePackingItemForList);
+    app.delete('/activities/:listId', staticContent.deletePackingList);
+    app.delete('/activities/:listId/items/:itemId', staticContent.deletePackingItemFromList);
+
+    // transport routes
+    app.get('/transport', staticContent.getPackingListsOfType("transport"));
+    app.get('/transport/:listId', staticContent.getSinglePackingList);
+    app.get('/transport/:listId/items', staticContent.getItemsForPackingList);
+    app.post('/transport', staticContent.createPackingListOfType("transport"));
+    app.post('/transport/:listId/items', staticContent.createPackingItemForList);
+    app.patch('/transport/:listId', staticContent.updatePackingList);
+    app.patch('/transport/:listId/items/:itemId', staticContent.updatePackingItemForList);
+    app.delete('/transport/:listId', staticContent.deletePackingList);
+    app.delete('/transport/:listId/items/:itemId', staticContent.deletePackingItemFromList);
+
+    // triptype routes
+    app.get('/triptypes', staticContent.getPackingListsOfType("triptype"));
+    app.get('/triptypes/:listId', staticContent.getSinglePackingList);
+    app.get('/triptypes/:listId/items', staticContent.getItemsForPackingList);
+    app.post('/triptypes', staticContent.createPackingListOfType("triptype"));
+    app.post('/triptypes/:listId/items', staticContent.createPackingItemForList);
+    app.patch('/triptypes/:listId', staticContent.updatePackingList);
+    app.patch('/triptypes/:listId/items/:itemId', staticContent.updatePackingItemForList);
+    app.delete('/triptypes/:listId', staticContent.deletePackingList);
+    app.delete('/triptypes/:listId/items/:itemId', staticContent.deletePackingItemFromList);
+
+    // weather routes
+    app.get('/weather', staticContent.getPackingListsOfType("weather"));
+    app.get('/weather/:listId', staticContent.getSinglePackingList);
+    app.get('/weather/:listId/items', staticContent.getItemsForPackingList);
+    app.post('/weather', staticContent.createPackingListOfType("weather"));
+    app.post('/weather/:listId/items', staticContent.createPackingItemForList);
+    app.patch('/weather/:listId', staticContent.updatePackingList);
+    app.patch('/weather/:listId/items/:itemId', staticContent.updatePackingItemForList);
+    app.delete('/weather/:listId', staticContent.deletePackingList);
+    app.delete('/weather/:listId/items/:itemId', staticContent.deletePackingItemFromList);
 }
