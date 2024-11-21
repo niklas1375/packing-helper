7787f7cb29d3ffb1a6e74e1ed3a6a5edd049c93c
Niklas <niklas1375@gmail.com>
2024-11-21 14:53:25 +0100

    move to word as key

---
+++ http_tests/sandbox.sql
@@ -0,0 +1,4 @@
+-- SQLite
+UPDATE PackingList set id = 'sunny' where id = 'cm0167ztc006jdlneperyc9zk';
+UPDATE PackingList set id = 'warm' where id = 'cm0167ztd006ndlnegyoem91r';
+UPDATE PackingList set id = 'wet' where id = 'cm0167ztf006udlneg02yiqhy';
